e450eb486b46b74904f177dad13da391b4afeac5
NehaArora321 <70069247+NehaArora321@users.noreply.github.com>
2021-01-22 23:56:19 +0530

    Update Game.js

--- js/Game.js
+++ js/Game.js
@@ -108,13 +108,14 @@ class Game{
     }
 
     leaderboard(){
-        strokeWeight(3)
-        stroke("red")
-        fill("White")
-        textSize(45)
-        text("And the winner is !!",200,100)
+        form.hide();
+        strokeWeight(3);
+        stroke("red");
+        fill("White");
+        textSize(45);
+        text("And the winner is !!",200,100);
         Player.getPlayerInfo();
-        textSize(75)
+        textSize(75);
         for(var plr in allPlayers){
             if(allPlayers[plr].score >= 10 ){
                 text(allPlayers[plr].name,300,250);
